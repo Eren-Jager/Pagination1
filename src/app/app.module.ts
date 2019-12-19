83725f999a1425a9a40053efe1144a032555159f
Eren-Jager <andechandra@gmail.com>
2019-12-19 17:43:19 +0530

    2 files modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,13 @@ import {PokemonListComponent} from './pokemon-list/pokemon-list.component';
 import {PokemonEntryComponent} from './pokemon-list/pokemon-entry/pokemon-entry.component';
 import {PaginationComponent} from './shared/pagination/pagination.component';
 import {RouterModule} from '@angular/router';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { LoaderComponent } from './shared/loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonListComponent,
     PokemonEntryComponent,
-    PaginationComponent,
-    NavbarComponent,
-    LoaderComponent
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
